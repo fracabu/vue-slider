42ff20e37b954e72dc471a2f1c6eb3cdb9871dc7
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-10 16:22:12 +0200

    modifiche

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const slides = [
+    {
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        image: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum.',
+    },
+    {
+        image: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+    },
+    {
+        image: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+    }
+];
+
+console.log(slides);
+
+const appVue = new Vue({
+	// selettore dell'elemento html dove avviare vue
+    el: "#app",
+
+  // Conterrà tutti i dati necessari all'applicazione
+    data: {
+	  // varie chiavi necessarie all'app
+    },
+	
+	// Conterrà una serie di funzioni
+	methods: {
+		// funzioni che useremo nell'app
+	}
+});
